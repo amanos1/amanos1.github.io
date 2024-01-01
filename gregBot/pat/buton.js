fbee3fb154d5756bbe6c5870bac263bd6f6e0bc6
Aaron <ajbrowne0622@yahoo.com>
2024-01-01 09:34:10 -0500

    added pat version

---
+++ gregBot/pat/buton.js
@@ -0,0 +1,79 @@
+document.getElementById("butt1").addEventListener("click", () => {
+  let dawg = new Audio("raw/ongod.m4a");
+  dawg.play();
+});
+
+document.getElementById("butt2").addEventListener("click", () => {
+  let beer = new Audio("raw/sohot.m4a");
+  beer.play();
+});
+
+document.getElementById("butt3").addEventListener("click", () => {
+  let twinkmoder = new Audio("raw/newguy.m4a");
+  twinkmoder.play();
+});
+
+document.getElementById("butt4").addEventListener("click", () => {
+  let windex = new Audio("raw/guysimgay.m4a");
+  windex.play();
+});
+
+document.getElementById("butt5").addEventListener("click", () => {
+  let worlstar = new Audio("raw/yt.m4a");
+  worlstar.play();
+});
+
+document.getElementById("butt6").addEventListener("click", () => {
+  let damn = new Audio("raw/hymen.m4a");
+  damn.play();
+});
+
+document.getElementById("butt7").addEventListener("click", () => {
+  let awesome = new Audio("raw/okay.m4a");
+  awesome.play();
+});
+
+document.getElementById("butt8").addEventListener("click", () => {
+  let drip = new Audio("raw/toilet.m4a");
+  drip.play();
+});
+
+document.getElementById("butt9").addEventListener("click", () => {
+  let mrpussy = new Audio("raw/gyood.m4a");
+  mrpussy.play();
+});
+
+document.getElementById("butt10").addEventListener("click", () => {
+  let meat = new Audio("raw/fit.m4a");
+  meat.play();
+});
+
+document.getElementById("butt11").addEventListener("click", () => {
+  let gender = new Audio("raw/weed.m4a");
+  gender.play();
+});
+
+document.getElementById("butt12").addEventListener("click", () => {
+  let slutthots = new Audio("raw/fiona.m4a");
+  slutthots.play();
+});
+
+document.getElementById("butt13").addEventListener("click", () => {
+  let dwboutit = new Audio("raw/regular.m4a");
+  dwboutit.play();
+});
+
+document.getElementById("butt14").addEventListener("click", () => {
+  let popsicle = new Audio("raw/mommy.m4a");
+  popsicle.play();
+});
+
+document.getElementById("butt15").addEventListener("click", () => {
+  let meowfuckouttheway = new Audio("raw/boozed.m4a");
+  meowfuckouttheway.play();
+});
+
+document.getElementById("butt16").addEventListener("click", () => {
+  let fucknobaby = new Audio("raw/smtiv.m4a");
+  fucknobaby.play();
+});
